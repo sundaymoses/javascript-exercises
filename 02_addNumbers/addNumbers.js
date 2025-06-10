8217e51d369c5139ffbc198f3edb12a456170778
sundaymoses <sandemoses2019@gmail.com>
2025-06-10 11:03:21 +0300

    completed the add numbers task

--- 02_addNumbers/addNumbers.js
+++ 02_addNumbers/addNumbers.js
@@ -4,10 +4,22 @@ function addNumbers() {
 
   let result;
 
-  result = "a" + "b"; // <------ EDIT THIS LINE
+  result = a + b; // <------ EDIT THIS LINE
 
   return result;
 }
 
 // Do not change this
 module.exports = addNumbers;
+
+
+
+
+// function addNumber(){
+//   const a=1;
+//   const b=1;
+//   let result; //using let so that we can change the vailue of results any time
+//   result = a + b;
+//   return result;
+// }
+// module.exports=addNumbers;
