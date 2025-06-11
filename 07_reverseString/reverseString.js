e35c3d69a04993d300e9654e2570adbfef8aead8
sundaymoses <sandemoses2019@gmail.com>
2025-06-11 11:45:38 +0300

    completed the reverseString solution

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(str) {
+  let  returnedStr="";
+  let finalReturn="";
+  let str1=str.split("");
+//  returnedStr=str.split("").reverse().join("");
+for (let i = str1.length-1; i >=0; i--) {
+   returnedStr += str1[i];
+   finalReturn=returnedStr;
+   
+}
+return finalReturn;
+// console.log(finalReturn);
+}
+// reverseString("hey there");
 // Do not edit below this line
+
 module.exports = reverseString;
