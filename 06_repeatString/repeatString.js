c33f6c7556cd07323e3d197e8afc6b58085f7eb0
sundaymoses <sandemoses2019@gmail.com>
2025-06-11 10:03:56 +0300

    completed the repeatString task

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    let stri = '';
+    if (times<0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < times; i++) {
+        stri += str
+    
+    }
+//   const strString=String(str);
+//   console.log(strString);
+//  const finalResult= str.repeat(times);
+//  return finalResult;
+// console.log(stri)
+return stri;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
