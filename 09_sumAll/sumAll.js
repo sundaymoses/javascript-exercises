3d3ed73fa9a17fd7da4e94c609466c850fd497a3
sundaymoses <sandemoses2019@gmail.com>
2025-07-01 16:01:04 +0300

    sum of the numbers corrected and working fine

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+//this functions finds the sum of the digits brought inside as arguments
+const sumAll = function(a,b) {
 
-};
-
-// Do not edit below this line
+    //convert a &b to numbers
+    let num1= a;
+    let num2= b;
+// if(!Number.isInteger(a))
+       if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+       if (num1 <0 || num2<0 ) return "ERROR";
+       if(num1 > num2){
+        let temp =num1;
+        num1= num2;
+        num2 =temp;
+       }
+let sum=0;
+for (let i = num1; i <= num2; i++) {
+ sum +=i;
+}
+ return sum;
+}
 module.exports = sumAll;
