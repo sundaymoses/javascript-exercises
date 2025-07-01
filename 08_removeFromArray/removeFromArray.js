c5fa7e3a962f4e668aa9f56b2a2bc467c8affe4b
sundaymoses <sandemoses2019@gmail.com>
2025-07-01 14:32:46 +0300

    completed the array removal function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+//this code expect us to fiter out array elements that appears in the arguments passed
+//we expect 2 arguments: the array, and the values that will be filtered from the array
+
+const removeFromArray = function(array, ...args) {
+//create a new array to store array that does not contain the filtered values
+let arr= [];
+//now fiter out the arrays using the args
+array.forEach((item)=>{
+    //im using ! to say "if it is not in the array, push it to the new array"
+    if(!args.includes(item)){
+        arr.push(item);
+    }
+})
+return arr;
 };
 
 // Do not edit below this line
